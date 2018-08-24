2079316ee0d655b8cb4149f096abb0d2197cc5db
Ljferrer <lukasjferrer@gmail.com>
2018-08-24 12:50:45 -0700

    Gave faiss_new_sample_index a .index file extension (noncritical, but recommended by faiss)

--- digtextsimilaritysearch/config.py
+++ digtextsimilaritysearch/config.py
@@ -1,5 +1,5 @@
 config = {
-    "faiss_index_path": "../data/testing/faiss_new_sample_index",
+    "faiss_index_path": "../data/testing/faiss_new_sample_index.index",
     "hbase_server": "localhost",
     "host": "0.0.0.0",
     "port": "5555"

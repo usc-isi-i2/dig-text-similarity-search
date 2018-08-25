7cdf1f355e0d6e960ad012d0fa7bfb2268bf4f59
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-08-24 19:19:49 -0700

    change signature of base key value class to suport tables

--- digtextsimilaritysearch/storage/key_value_storage.py
+++ digtextsimilaritysearch/storage/key_value_storage.py
@@ -2,8 +2,8 @@ class KeyValueStorage(object):
     def __init__(self):
         pass
 
-    def get_record(self, record_id):
+    def get_record(self, record_id, table_name):
         raise NotImplementedError
 
-    def insert_record(self, record_id, record):
+    def insert_record(self, record_id, record, table_name):
         raise NotImplementedError
--- digtextsimilaritysearch/storage/memory_storage.py
+++ digtextsimilaritysearch/storage/memory_storage.py
@@ -4,10 +4,14 @@ from digtextsimilaritysearch.storage.key_value_storage import KeyValueStorage
 class MemoryStorage(KeyValueStorage):
     def __init__(self):
         KeyValueStorage.__init__(self)
-        self._records = dict
+        self._db = dict()
 
-    def get_record(self, record_id):
-        return self._records.get(record_id, None)
+    def get_record(self, record_id, table_name):
+        if table_name in self._db:
+            return self._db[table_name].get(record_id, None)
+        return None
 
-    def insert_record(self, record_id, record):
-        self._records[record_id] = record
+    def insert_record(self, record_id, record, table_name):
+        if table_name not in self._db:
+            self._db[table_name] = dict()
+        self._db[table_name][record_id] = record

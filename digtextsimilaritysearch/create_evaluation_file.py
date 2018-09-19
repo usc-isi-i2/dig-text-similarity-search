db39d3af46a4b1dd6745bf01bc13393ed63e1f9c
saggu <amandeep.s.saggu@gmail.com>
2018-09-18 18:32:48 -0700

    add options for running the evaluation code in a script

--- digtextsimilaritysearch/create_evaluation_file.py
+++ digtextsimilaritysearch/create_evaluation_file.py
@@ -2,16 +2,17 @@ from vectorizer.sentence_vectorizer import SentenceVectorizer
 from indexer.faiss_indexer import FaissIndexer
 from storage.es_adapter import ESAdapter
 from process_documents.document_processor import DocumentProcessor
-from config import config
 
-import os
 import json
 import requests
 from time import time
 import pandas as pd
+from optparse import OptionParser
 
 es_url = 'http://dig:dIgDiG@mydig-sage-internal.isi.edu:80/es/'
 es_index = 'sage_news'
+table = 'dig-text-similarity-search'
+faiss_path = '../data/testing/faiss_new_sample_index.index'
 
 ids_query_str = """{
   "query": {
@@ -21,44 +22,53 @@ ids_query_str = """{
   }
 }"""
 
-cwd = os.getcwd()
-doc_file = '../data/testing/news_sample.jl'
-doc_file_path = os.path.join(cwd, doc_file)
-docs = [json.loads(x) for x in open(doc_file_path)]
+if __name__ == '__main__':
+    option_parser = OptionParser()
+    option_parser.add_option('-q', '--query', dest='query')
+    option_parser.add_option('-o', '--output', dest='output', default='/tmp/evaluation.csv')
+    option_parser.add_option('-f', '--faiss', dest='faiss', help='path to faiss index', default=faiss_path)
+    option_parser.add_option('-t', '--table', dest='table', help='faiss id to sentence id mapping table', default=table)
+    option_parser.add_option('-e', '--es', dest='es', default=es_url)
+    option_parser.add_option('-i', '--index', dest='index', default=es_index)
+    (opts, args) = option_parser.parse_args()
+    ifp = opts.query
+    output = opts.output
+    faiss = opts.faiss
+    table = opts.table
+    es_url = opts.es
+    es_index = opts.index
 
-fi = FaissIndexer(path_to_index_file=config['faiss_index_path'])
-sentence_vectorizer = SentenceVectorizer()
-es_adapter = ESAdapter()
+    fi = FaissIndexer(faiss)
+    sentence_vectorizer = SentenceVectorizer()
+    es_adapter = ESAdapter()
 
-dp = DocumentProcessor(fi, sentence_vectorizer, es_adapter, table_name='dig-text-similarity-search')
+    dp = DocumentProcessor(fi, sentence_vectorizer, es_adapter, table_name=table)
 
-ifp = 'what is the moving annual return'
+    start_time = time()
+    results = dp.query_text(ifp)
+    time_taken = time() - start_time
 
-start_time = time()
-results = dp.query_text(ifp)
-time_taken = time() - start_time
+    doc_ids = [x['doc_id'] for x in results]
+    ids_query = json.loads(ids_query_str)
+    ids_query['query']['ids']['values'] = doc_ids
 
-doc_ids = [x['doc_id'] for x in results]
-ids_query = json.loads(ids_query_str)
-ids_query['query']['ids']['values'] = doc_ids
+    es_response = requests.post('{}/_search'.format(es_url), json=ids_query)
 
-es_response = requests.post('{}/_search'.format(es_url), json=ids_query)
+    es_results = es_response.json()['hits']['hits']
 
-es_results = es_response.json()['hits']['hits']
+    doc_dict = {}
+    for hit in es_results:
+        doc_dict[hit['_id']] = hit['_source']['knowledge_graph']['description'][0]['value']
 
-doc_dict = {}
-for hit in es_results:
-    doc_dict[hit['_id']] = hit['_source']['knowledge_graph']['description'][0]['value']
+    evaluation_list = list()
 
-evaluation_list = list()
+    for result in results:
+        doc_id = result['doc_id']
+        evaluation_list.append(
+            (ifp, doc_id, doc_dict[doc_id], result['sentence_id'], result['sentence'], result['score'],
+             str(time_taken), -1))
 
-for result in results:
-    doc_id = result['doc_id']
-    evaluation_list.append((ifp, doc_id, doc_dict[doc_id], result['sentence_id'], result['sentence'], result['score'],
-                            str(time_taken), -1))
-
-df = pd.DataFrame(data=evaluation_list,
-                  columns=['ifp', 'doc_id', 'doc_text', 'sentence_id', 'sentence_text', 'score', 'query_time',
-                           'relevance'])
-df.to_csv('../data/evaluation_file.csv', index=False)
-# print(json.dumps(evaluation_list, indent=2))
+    df = pd.DataFrame(data=evaluation_list,
+                      columns=['ifp', 'doc_id', 'doc_text', 'sentence_id', 'sentence_text', 'score', 'query_time',
+                               'relevance'])
+    df.to_csv(output, index=False)

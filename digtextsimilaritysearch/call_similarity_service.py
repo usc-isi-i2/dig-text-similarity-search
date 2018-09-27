e2d1d786ed9e43f0c7df303e3c7de20bfcd6f16b
Ljferrer <lukasjferrer@gmail.com>
2018-09-26 17:41:01 -0700

    For testing

--- digtextsimilaritysearch/call_similarity_service.py
+++ digtextsimilaritysearch/call_similarity_service.py
@@ -1,5 +1,5 @@
 import requests
-local_url = 'http://localhost:5555/search'
+local_url = 'http://localhost:5954/search'
 payload = {'query': 'what is the moving annual return'}
 r = requests.get(local_url, params=payload)
 print(r.text)

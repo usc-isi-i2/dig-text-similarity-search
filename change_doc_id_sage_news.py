c43fc26f7a10685c1b13757701af760de63e461b
saggu <amandeep.s.saggu@gmail.com>
2018-10-10 15:36:38 -0700

    doc id from the source

--- change_doc_id_sage_news.py
+++ change_doc_id_sage_news.py
@@ -1,6 +1,6 @@
-from kafka import KafkaProducer
+# from kafka import KafkaProducer
 from kafka import KafkaConsumer
-import json
+# import json
 import datetime
 
 broker_list = [
@@ -14,12 +14,12 @@ news_output_path = '/data/sage_news_backup'
 
 todays_date = str(datetime.date.today())
 news_output_file = open('{}/{}.jl'.format(news_output_path, todays_date), mode='a', encoding='utf-8')
-
-producer = KafkaProducer(
-    bootstrap_servers=broker_list,
-    value_serializer=lambda v: v.encode('utf-8'),
-    **args
-)
+#
+# producer = KafkaProducer(
+#     bootstrap_servers=broker_list,
+#     value_serializer=lambda v: v.encode('utf-8'),
+#     **args
+# )
 
 consumer = KafkaConsumer(
     'sage_news_v2_out',
@@ -33,15 +33,17 @@ consumer = KafkaConsumer(
 def read_message(consumer):
     for msg in consumer:
         try:
-            doc = json.loads(msg.value.decode('utf-8'))
+            # doc = json.loads(msg.value.decode('utf-8'))
+            doc = msg.value.decode('utf-8')
+            news_output_file.write(doc + '\n')
             # infinite loop alert
-            if doc.get('type', '') != 'sage_news_v2':
-                doc['type'] = 'sage_news_v2'
-                doc_str = json.dumps(doc)
-
-                r = producer.send('sage_news_v2_out', doc_str)
-                r.get(timeout=60)
-                news_output_file.write(doc_str + '\n')
+            # if doc.get('type', '') != 'sage_news_v2':
+            #     doc['type'] = 'sage_news_v2'
+            #     doc_str = json.dumps(doc)
+            #
+            #     r = producer.send('sage_news_v2_out', doc_str)
+            #     r.get(timeout=60)
+            #     news_output_file.write(doc_str + '\n')
 
         except:
 

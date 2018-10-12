bebd102fdc30ee84ee6f0f7b97bf4500080e4b3b
Ljferrer <lukasjferrer@gmail.com>
2018-10-12 16:49:17 -0700

    add_shard() is now an importable function

--- digtextsimilaritysearch/add_shard_similarity_service.py
+++ digtextsimilaritysearch/add_shard_similarity_service.py
@@ -2,10 +2,18 @@ import requests
 from optparse import OptionParser
 
 options = OptionParser()
-options.add_option('-i', '--index_path')
+options.add_option('-i', '--index_path', default=None)
 (opts, _) = options.parse_args()
 
-local_url = 'http://localhost:5954/faiss'
-payload = {'path': opts.index_path}
-r = requests.get(local_url, params=payload)
-print(r.text)
+
+def add_shard(path=opts.index_path, local_url='http://localhost:5954/faiss'):
+    payload = {'path': path}
+    r = requests.get(local_url, params=payload)
+    print(r.text)
+
+
+if __name__ == '__main__':
+    if opts.index_path:
+        add_shard(path=opts.index_path)
+    else:
+        print('Please provide path to new shard with: -i /path/to/shard.index')

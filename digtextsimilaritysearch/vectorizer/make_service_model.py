079be6240bb874c9c029ae8187863b58f9f1b6f1
Ljferrer <lukasjferrer@gmail.com>
2018-10-17 18:45:16 -0700

    Uses local model

--- digtextsimilaritysearch/vectorizer/make_service_model.py
+++ digtextsimilaritysearch/vectorizer/make_service_model.py
@@ -1,10 +1,17 @@
+import os
 import tensorflow as tf
 import tensorflow_hub as hub
 
-MODEL_NAME = 'USE-liteQuery-v1'
-MODEL_LINK = 'https://tfhub.dev/google/universal-sentence-encoder/1'
+model_dir = os.path.join(os.path.dirname(__file__),
+                         'model/1fb57c3ffe1a38479233ee9853ddd7a8ac8a8c47')
+if os.path.isdir(model_dir):
+    model_link = model_dir
+else:
+    model_link = 'https://tfhub.dev/google/universal-sentence-encoder/2'
+MODEL_LINK = model_link
+MODEL_NAME = 'USE-liteQuery-v2'
 VERSION = '001'
-SERVE_PATH = './service/{}/{}'.format(MODEL_NAME, VERSION)
+SERVE_PATH = './service_models/{}/{}'.format(MODEL_NAME, VERSION)
 
 with tf.Graph().as_default():
     module = hub.Module(MODEL_LINK, name=MODEL_NAME)

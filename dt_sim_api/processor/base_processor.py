e5fcd874c03f36c7063d4a22f4af564770350196
Ljferrer <lukasjferrer@gmail.com>
2018-11-15 12:08:43 -0800

    New pythonic joint_sort()

--- dt_sim_api/processor/base_processor.py
+++ dt_sim_api/processor/base_processor.py
@@ -13,39 +13,24 @@ class BaseProcessor(object):
     def vectorize(self, text: Union[str, List[str]]):
         raise NotImplementedError
 
-    # noinspection PyTypeChecker
     @staticmethod
     def joint_sort(scores: List[List[float]], ids: List[List[int]]
                    ) -> Tuple[List[List[float]], List[List[int]]]:
         """
-        Sorts scores in ascending order while maintaining score::id mapping. 
-        Checks if input is already sorted. 
-        
-        :param scores: 
-        :param ids: Corresponding ids
+        Sorts scores in ascending order while maintaining score::id mapping.
+        Checks if input is already sorted.
+        :param scores: Faiss query/hit vector L2 distances
+        :param ids: Corresponding faiss vector ids
         :return: Scores sorted in ascending order with corresponding ids
         """
         # Check
-        if all(scores[i] <= scores[i+1] for i in range(len(scores)-1)):
+        if all(scores[0][i] <= scores[0][i+1] for i in range(len(scores[0])-1)):
             return scores, ids
 
-        # TODO: try zipped sort for more simplicity
-        # Sort
-        results = dict()
-        for score, sent_id in zip(scores[0], ids[0]):
-            if score not in results:
-                results[score] = list()
-            results[score].append(sent_id)
-
-        consistent_results = OrderedDict(sorted(results.items()))
-        for score in consistent_results:
-            consistent_results[score].sort()
-
-        sorted_scores = list()
-        sorted_ids = list()
-        for score, sids in consistent_results.items():
-            for sent_id in sids:
-                sorted_scores.append(score)
-                sorted_ids.append(sent_id)
+        # Pythonic Joint Sort
+        if isinstance(scores[0], list) and isinstance(ids[0], list):
+            scores, ids = scores[0], ids[0]
+        sorted_scores, sorted_ids = (list(sorted_scs_ids) for sorted_scs_ids
+                                     in zip(*sorted(zip(scores, ids))))
 
         return [sorted_scores], [sorted_ids]

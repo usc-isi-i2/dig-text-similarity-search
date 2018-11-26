c273111fa2e9ed1452ba041fedf47f4cabb648d8
Ljferrer <lukasjferrer@gmail.com>
2018-11-26 12:30:10 -0800

    Now uses QueryProcessor (depricated DocumentProcessor)

--- scripts/service/similarity_server.py
+++ scripts/service/similarity_server.py
@@ -1,76 +1,113 @@
+# <editor-fold desc="Basic Imports">
 from __future__ import unicode_literals
 from flask import Flask, jsonify
 from flask import request
 from flask_cors import CORS
 
 import os
-import json
 import sys
+import json
 import traceback
 
-from indexer.IVF_disk_index_handler import DeployShards
-from process_documents.document_processor import DocumentProcessor
-from vectorizer.sentence_vectorizer import DockerVectorizer
-from storage.es_adapter import ESAdapter
-from config import config
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+# </editor-fold>
 
+from dt_sim_api.processor.query_processor import QueryProcessor
+from dt_sim_api.indexer.ivf_index_handlers import DeployShards, RangeShards
+from dt_sim_api.vectorizer.sentence_vectorizer import DockerVectorizer
+
+from scripts.configs.config import std_config, lrg_config
+
+from optparse import OptionParser
+options = OptionParser()
+options.add_option('-i', '--index_dir_path', type='str')
+options.add_option('-r', '--range_search', action='store_true', default=False)
+options.add_option('-l', '--large', action='store_true', default=False)
+options.add_option('-d', '--debug', action='store_true', default=False)
+options.add_option('-A', '--AWS', action='store_true', default=False)   # Internal
+(opts, _) = options.parse_args()
+
+
+##### CONFIGURE #####
+if opts.large:
+    my_config = lrg_config
+else:
+    my_config = std_config
+
+# Change index paths if necessary
+if opts.index_dir_path:
+    my_config['faiss_index_path'] = os.path.abspath(opts.index_dir_path)
+
+# Internal
+if opts.AWS:
+    my_config = lrg_config
+    my_config['faiss_index_path'] = '/faiss/news_day_shards/'
+
+
+##### INIT #####
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-print('Initializing Batch Vectorizer')
-query_vectorizer = DockerVectorizer()
+print('* Initializing Faiss Indexes')
+if opts.range_search:
+    faiss_indexer = RangeShards(my_config['faiss_index_path'])
+else:
+    faiss_indexer = DeployShards(my_config['faiss_index_path'])
 
-print('Initializing Faiss Indexer')
-faiss_indexer = DeployShards(config['faiss_index_path'])
+print('* Initializing Query Vectorizer')
+query_vectorizer = DockerVectorizer(large=my_config['large_emb_space'])
 
-print('Initializing ES Adapter')
-es_adapter = ESAdapter(es_endpoint=config['es_endpoint'])
-
-print('Initializing Document Processor')
-dp = DocumentProcessor(indexer=faiss_indexer, vectorizer=query_vectorizer,
-                       storage_adapter=None, table_name=config['es_index'])
+print('* Initializing Query Processor')
+qp = QueryProcessor(index_handler=faiss_indexer, query_vectorizer=query_vectorizer)
 
 
-@app.route("/")
+##### APP DEF #####
+@app.route('/')
 def hello():
-    return "DIG Text Similarity Search\n"
+    return 'DIG Text Similarity Search\n'
 
 
-@app.route("/search", methods=['GET'])
+@app.route('/search', methods=['GET'])
 def text_similarity_search():
-    query = request.args.get("query", None)
-    k = request.args.get("k", 10)
-    rerank_by_doc = request.args.get("rerank_by_doc", 'false')
-    rerank_by_doc = str(rerank_by_doc).lower() == 'true'
+    query = request.args.get('query', None)
+    k = request.args.get('k', 10)
+    # rerank_by_doc = request.args.get('rerank_by_doc', 'false')
+    # rerank_by_doc = str(rerank_by_doc).lower() == 'true'
     if not query:
-        return jsonify({"message": "The service is not able to process null request"}), 400
+        return jsonify({'message': 'The service is not able to process null requests'}), 400
 
     try:
-        results = dp.query_text(query, k=int(k), rerank_by_doc=rerank_by_doc)
+        results = qp.query_corpus(query, k=int(k))
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
         print(''.join(lines))
-        return jsonify({"message": str(e)}), 500
+        return jsonify({'message': str(e)}), 500
 
     return json.dumps(results), 200
 
 
-@app.route("/faiss", methods=['PUT'])
+@app.route('/faiss', methods=['PUT'])
 def add_shard():
-    shard_path = request.args.get("path", None)
+    shard_path = os.path.abspath(request.args.get('path', None))
     if not os.path.exists(shard_path):
-        return jsonify({"message": "Path does not exist: {}".format(shard_path)}), 404
+        return jsonify({'message': 'Path does not exist: {}'.format(shard_path)}), 404
 
     try:
-        dp.indexer.add_shard(shard_path)
-        return jsonify({'message': 'successfully added shard to faiss index'}), 201
+        qp.add_shard(shard_path)
+        return jsonify({'message': 'Successfully added shard to faiss index'}), 201
     except Exception as e:
-        return jsonify({"message": str(e)}), 500
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        print(''.join(lines))
+        return jsonify({'message': str(e)}), 500
 
 
+##### MAIN #####
 def main():
-    app.run(host=config['host'], port=config['port'], threaded=True, debug=False)
+    app.run(host=my_config['host'], port=my_config['port'],
+            threaded=True, debug=opts.debug)
 
 
 if __name__ == '__main__':

46d454d5e920a3ccf6f77b52df33a52cd8b4609d
Ljferrer <lukasjferrer@gmail.com>
2018-12-04 15:03:48 -0800

    Testing reranking methods

--- dt_sim_api/processor/query_processor.py
+++ dt_sim_api/processor/query_processor.py
@@ -12,6 +12,13 @@ from dt_sim_api.vectorizer.sentence_vectorizer import DockerVectorizer
 
 __all__ = ['QueryProcessor']
 
+DiffScores = List[List[np.float32]]
+VectIDList = List[List[np.int64]]
+SentHitList = List[Tuple[np.float32, np.int64]]
+DocPayload = Dict[str, SentHitList]
+
+FinalOutput = List[Dict[str, str]]
+
 
 class QueryProcessor(BaseProcessor):
 
@@ -24,13 +31,14 @@ class QueryProcessor(BaseProcessor):
         self.vectorizer = query_vectorizer
 
     @Memoized
-    def query_corpus(self, query_str: str, k: int = 5, 
-                     verbose: bool = True) -> List[dict]:
+    def query_corpus(self, query_str: str, k: int = 5, score_type: int = None,
+                     verbose: bool = True) -> FinalOutput:
         """
         Vectorize query -> Search faiss index handler -> Format doc payload
         Expects to receive only one query per call.
         :param query_str: Query to vectorize
         :param k: Number of nearest neighboring documents to return
+        :param score_type: TESTING
         :param verbose: Prints time spent on each step
         :return: k sorted document hits
         """
@@ -46,8 +54,10 @@ class QueryProcessor(BaseProcessor):
         # Aggregate hits into docs -> rerank (soon) -> format
         t_p = time()
         doc_hits = self.aggregate_docs(scores, faiss_ids)
-        # self.rerank()     # TODO: implement new reranking logic
-        similar_docs = self.format_payload(doc_hits)
+        if score_type:
+            similar_docs = self.title_rerank(doc_hits, score_type)
+        else:
+            similar_docs = self.format_payload(doc_hits)
 
         t_r = time()
         if verbose:
@@ -75,48 +85,100 @@ class QueryProcessor(BaseProcessor):
         return query_vector
 
     @staticmethod
-    def aggregate_docs(scores: List[List[float]], faiss_ids: List[List[int]]
-                       ) -> Dict[str, List[Tuple[float, int]]]:
+    def aggregate_docs(scores: DiffScores, faiss_ids: VectIDList
+                       ) -> DocPayload:
         """
         Collects outputs from faiss search into document entities.
         :param scores: Faiss query/hit vector L2 distances
         :param faiss_ids: Faiss vector ids
         :return: Dict of docs (key: document id, val: doc with sentence hits)
         """
-        def min_diff_cutoff(diff_score, cutoff=0.1):
+        def min_diff_cutoff(diff_score, cutoff=0.01):
             return max(diff_score, cutoff)
         
         docs = dict()
         for score, faiss_id in zip(scores[0], faiss_ids[0]):
-            doc_id, sent_id = divmod(faiss_id, 10000)
-            doc_id = str(doc_id)
-            if doc_id not in docs:
-                docs[doc_id] = list()
-            docs[doc_id].append((min_diff_cutoff(score), faiss_id))
+            if faiss_id > 0:
+                doc_id, sent_id = divmod(faiss_id, 10000)
+                doc_id = str(doc_id)
+                if doc_id not in docs:
+                    docs[doc_id] = list()
+                docs[doc_id].append((min_diff_cutoff(score), faiss_id))
         return docs
 
     @staticmethod
-    def rerank():
-        pass
+    def format_payload(doc_hits: DocPayload) -> FinalOutput:
+        """
+        TMP payload formatting for current sandpaper implementation
 
-    @staticmethod
-    def format_payload(doc_hits: Dict[str, List[Tuple[float, int]]]
-                       ) -> List[Dict[str, str]]:
-        """ 
-        TMP payload formatting for current sandpaper implementation 
-        
-        Old payload structure: 
+        Old payload structure:
             [ { 'score': str(faiss_diff), 'sentence_id': str(faiss_id) } ]
         """
         payload = list()
         for doc_id, faiss_diff_ids in doc_hits.items():
-            if int(doc_id) > 0:
-                for faiss_diff, faiss_id in faiss_diff_ids:
-                    out = dict()
-                    out['score'] = str(faiss_diff)
-                    out['sentence_id'] = str(faiss_id)
-                    payload.append(out)
-        return sorted(payload, key=lambda h: h['score'])
+            for faiss_diff, faiss_id in faiss_diff_ids:
+                out = dict()
+                out['score'] = str(faiss_diff)
+                out['sentence_id'] = str(faiss_id)
+                payload.append(out)
+        return sorted(payload, key=lambda sc_id: sc_id['score'])
+
+    @staticmethod
+    def title_rerank(doc_hits: DocPayload, new_score_type: int) -> FinalOutput:
+        titles = dict()
+        for doc_id, faiss_diff_ids in doc_hits.items():
+            for i, (_, faiss_id) in enumerate(faiss_diff_ids):
+                if str(faiss_id).endswith('0000'):
+                    faiss_diff_ids.append(faiss_diff_ids.pop(i))
+                    titles[doc_id] = faiss_diff_ids
+                    break
+
+        def hard_score(score, hits: SentHitList):
+            n_hits = len(hits)
+            boosted_score = score / n_hits
+            return boosted_score
+
+        def soft_score2(title_score, hits: SentHitList):
+            avg_score = (title_score + hits[0][0])/2
+            boosted_score = avg_score / len(hits)
+            return boosted_score
+
+        def soft_score(all_hits: SentHitList):
+            avg_score = sum(all_hits[j][0] for j in range(len(all_hits)))/len(all_hits)
+            boosted_score = avg_score / len(all_hits)
+            return boosted_score
+
+        reranked = list()
+        for doc_id, faiss_diff_ids in titles.items():
+            title_diff, title_id = faiss_diff_ids.pop(-1)
+            faiss_diff_ids.sort()
+
+            out = dict()
+            # By title
+            if new_score_type == 0:
+                out['score'] = hard_score(title_diff, faiss_diff_ids)
+                out['sentence_id'] = str(title_id)
+            # By content
+            elif new_score_type == 1:
+                best_sent = faiss_diff_ids[0][0]
+                out['score'] = hard_score(best_sent, faiss_diff_ids)
+            # Avg title+content
+            elif new_score_type == 2:
+                faiss_diff_ids.append((title_diff, title_id))
+                out['score'] = soft_score(faiss_diff_ids)
+            # Avg title+best
+            elif new_score_type == 3:
+                out['score'] = soft_score2(title_diff, faiss_diff_ids)
+            # Default
+            else:
+                faiss_diff_ids.append((title_diff, title_id))
+                faiss_diff_ids.sort()
+                out['score'] = faiss_diff_ids[0][0]
+            if 'sentence_id' not in out:
+                out['sentence_id'] = str(faiss_diff_ids[0][1])
+            reranked.append(out)
+
+        return sorted(reranked, key=lambda sc_id: sc_id['score'])
 
     def add_shard(self, shard_path: str):
         """

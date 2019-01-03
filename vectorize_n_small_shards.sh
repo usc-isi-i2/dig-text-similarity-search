d486400c0f9b239c7f1fdeefb876922ae56044bf
Lukas Ferrer <lukasjferrer@gmail.com>
2019-01-03 14:39:33 -0800

    Updated variable defaults

--- vectorize_n_small_shards.sh
+++ vectorize_n_small_shards.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
+
+n_shards=$1
+input_dir=$2
+output_dir=$3
+progress_txt=${4:-progress.txt}
+
+
 printf "\n
-Arg1: n shards to process: $1
-Arg2: input file dir:      $2
-Arg3: output index dir:    $3
-Arg4: using progress file: ${4:progress.txt}
+Arg1: n shards to process: $n_shards
+Arg2: input file dir:      $input_dir
+Arg3: output index dir:    $output_dir
+Arg4: using progress file: $progress_txt
 \n"
 
-printf "\nPreprocessing $1 shard(s) with small Universal Sentence Encoder\n";
-for i in `seq 1 $1`
+
+printf "\nPreprocessing $n_shards shard(s) with small Universal Sentence Encoder\n";
+
+for i in `seq 1 "$n_shards"`
 do
-	printf "\n\nStarting $i/$1 @ $(date)\n";
-	python -u py_scripts/preprocessing/prep_shard.py -i $2 -o $3 \
+	printf "\n\nStarting $i/$n_shards @ $(date)\n";
+	python -u py_scripts/preprocessing/prep_shard.py -i "$input_dir" -o "$output_dir" \
 	-b base_indexes/USE_lite_base_IVF16K.index \
-	-p $4 -t 2 -r -d;
+	-p "$progress_txt" -t 2 -r -d;
 done
 
-printf "\n\n\nCompleted @ $(date)\n";
+printf "\n\n\nCompleted @ $(date)\n\n";

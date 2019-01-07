1660c8d02a0558281b946e3895fada58d5923aa6
Ljferrer <lukasjferrer@gmail.com>
2019-01-06 16:51:44 -0800

    Sorts out pub dates beyond current date

--- dt_sim/data_reader/date_sort_funcs.py
+++ dt_sim/data_reader/date_sort_funcs.py
@@ -1,7 +1,9 @@
+import re
 import os
 import os.path as p
 import json
 from time import time
+from datetime import date
 
 __all__ = ['pub_date_split']
 
@@ -21,12 +23,17 @@ def pub_date_split(input_file: str, output_dir: str,
     if not p.isdir(output_dir):
         os.mkdir(output_dir)
 
-    old_news = p.join(output_dir, 'old_news')
-    if not p.isdir(old_news):
-        os.mkdir(old_news)
+    old_news_dir = p.join(output_dir, 'old_news')
+    if not p.isdir(old_news_dir):
+        os.mkdir(old_news_dir)
+
+    date_error_dir = p.join(output_dir, 'date_error')
+    if not p.isdir(date_error_dir):
+        os.mkdir(date_error_dir)
 
     t_0 = time()
-    new, old, dateless = 0, 0, 0
+    date_today = str(date.today())
+    new, old, err, dateless = 0, 0, 0, 0
     with open(input_file, 'r') as srcf:
         for line in srcf:
             article = json.loads(line)
@@ -36,18 +43,23 @@ def pub_date_split(input_file: str, output_dir: str,
             except KeyError:
                 event_date = None
 
-            if event_date and event_date >= cutoff_date:
+            if event_date and date_today >= event_date >= cutoff_date:
                 targetf = p.join(output_dir, f'{event_date}.jl')
                 new += 1
-            elif event_date:
-                targetf = p.join(old_news, f'{event_date}.jl')
+            elif event_date and event_date < cutoff_date:
+                targetf = p.join(old_news_dir, f'{event_date}.jl')
                 old += 1
+            elif event_date and date_today < event_date:
+                targetf = p.join(date_error_dir, f'{event_date}.jl')
+                err += 1
             else:
-                targetf = p.join(old_news, 'dateless_articles.jl')
+                targetf = p.join(date_error_dir, 'dateless_articles.jl')
                 dateless += 1
 
             with open(targetf, 'a') as trgf:
                 trgf.write(f'{json.dumps(article)}\n')
 
-    print(f'Sorted {new+old} files in {time()-t_0:0.1f}s '
-          f'({100*new/(new+old):0.1f}% published since {cutoff_date})')
+    m, s = divmod(time()-t_0, 60)
+    print(f'Sorted {new+old+err+dateless} files in {m:2d}m{s:0.1f}s '
+          f'({100*new/(new+old+err+dateless):0.1f}% published '
+          f'between {date_today} and {cutoff_date})')

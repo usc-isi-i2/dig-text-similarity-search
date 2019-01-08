7d0adbc6ab03f57a13ff6f2bbf1c4e6a12871f59
Lukas Ferrer <lukasjferrer@gmail.com>
2019-01-07 20:24:26 -0800

    Reports time spent filtering sources

--- dt_sim/data_reader/source_filter_funcs.py
+++ dt_sim/data_reader/source_filter_funcs.py
@@ -1,5 +1,6 @@
 import json
 import os.path as p
+from time import time
 from typing import Union, List, Tuple
 
 
@@ -21,6 +22,7 @@ except FileNotFoundError:
 
 def source_filter(input_file, output_file,
                   white_list: Union[List, Tuple] = news_white_list):
+    t_0 = time()
     n_files, m_good = 0, 0
     with open(input_file, 'r') as src, open(output_file, 'a') as dst:
         for line in src:
@@ -29,5 +31,7 @@ def source_filter(input_file, output_file,
             if doc['lexisnexis']['metadata']['source'] in white_list:
                 m_good += 1
                 dst.write(json.dumps(doc) + '\n')
-    print(f'{n_files} files sorted '
+
+    m, s = divmod(time() - t_0, 60)
+    print(f'{n_files} files sorted in {int(m):2d}m{s:0.1f}s '
           f'({100*m_good/n_files:0.1f}% from trusted sources)')

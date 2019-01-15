442bc9eb831a8f14418bef8d182e29a40c87c662
Lukas Ferrer <lukasjferrer@gmail.com>
2019-01-15 15:07:15 -0800

    Added faiss index RangeSearch option

--- py_scripts/service/similarity_server.py
+++ py_scripts/service/similarity_server.py
@@ -27,12 +27,15 @@ arp.add_argument('-l', '--large', action='store_true',
                       'Note: Encoder and Faiss embedding spaces must match!')
 arp.add_argument('-d', '--debug', action='store_true', default=False,
                  help='Increases verbosity of Flask app.')
+arp.add_argument('-r', '--range_search', action='store_true', default=False,
+                 help='Performs query-vector proximity search within an '
+                      'L2 radius of 1.2 (instead of the default faiss k-search)')
 opts = arp.parse_args()
 # </editor-fold>
 
 
 from dt_sim.processor.query_processor import QueryProcessor
-from dt_sim.indexer.ivf_index_handlers import DeployShards
+from dt_sim.indexer.ivf_index_handlers import DeployShards, RangeShards
 from dt_sim.vectorizer.sentence_vectorizer import DockerVectorizer
 
 from py_scripts.configs.config import std_config, lrg_config
@@ -54,9 +57,14 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
 print(' * Initializing Faiss Indexes')
-faiss_indexer = DeployShards(my_config['faiss_index_path'], nprobe=opts.centroids)
+if opts.range_search:
+    faiss_indexer = RangeShards(my_config['faiss_index_path'],
+                                nprobe=opts.centroids)
+else:
+    faiss_indexer = DeployShards(my_config['faiss_index_path'],
+                                 nprobe=opts.centroids)
 
-print(' * Initializing Query Vectorizer')
+print(' * Initializing Query Vectorizer')   # Emb space Bool toggle
 query_vectorizer = DockerVectorizer(large=my_config['large_emb_space'])
 
 print(' * Initializing Query Processor')

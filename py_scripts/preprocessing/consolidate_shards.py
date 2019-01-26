bf38bd8c787051752c1d15cf753d7aee1f84579e
Lukas Ferrer <lukasjferrer@gmail.com>
2019-01-25 18:05:55 -0800

    Updated usage

--- py_scripts/preprocessing/consolidate_shards.py
+++ py_scripts/preprocessing/consolidate_shards.py
@@ -10,9 +10,6 @@ sys.path.append(p.join(p.dirname(__file__), '../..'))
 # <editor-fold desc="Parse Command Line Options">
 arp = ArgumentParser(description='Use this script to move, copy, or zip/merge '
                                  'on-disk IVF faiss indexes from the command line. '
-                                 'Zipping (default behavior) automatically groups '
-                                 'and merges indexes that share an ISO-date in '
-                                 'their filenames. '
                                  'Note: Performing `$ mv my_IVF.index ...` from '
                                  'the command line will sever the link to its '
                                  'my_IVF.ivfdata file.')
@@ -22,16 +19,10 @@ arp.add_argument('mv_dir', help='Collect on-disk IVF indexes to manipulate from 
 arp.add_argument('to_dir', help='... place indexes (and corresponding '
                                 '.ivfdata files) into to_dir.')
 
-base_idx = arp.add_mutually_exclusive_group()
-base_dir_path = p.abspath(p.join(p.dirname(__file__), '../../base_indexes/'))
-large_base_path = p.join(base_dir_path, 'USE_large_base_IVF4K_15M.index')
-base_idx.add_argument('-b', '--base_index', default=large_base_path,
-                      help='')
-
-action = arp.add_mutually_exclusive_group()
-action.add_argument('-r', '--recursive', action='store_true', default=False,
-                    help='Flag to recursively collect faiss indexes '
-                         'nested within mv_dir. (Only available for zip/merging)')
+action = arp.add_mutually_exclusive_group(required=True)
+action.add_argument('-z', '--zip', action='store_true', default=False,
+                    help='Zipping automatically groups and merges indexes '
+                         'that share an ISO-date in their filenames.')
 action.add_argument('-m', '--mv', '--move', action='store_true', default=False,
                     help='Moves indexes from mv_dir to to_dir (does NOT zip '
                          'indexes together). Fails if existing filenames conflict! '
@@ -40,6 +31,22 @@ action.add_argument('-c', '--cp', '--copy', action='store_true', default=False,
                     help='Copies indexes from mv_dir to to_dir (does NOT delete '
                          'anything in mv_dir). Fails if existing filenames conflict! '
                          '(Note: may fail after partial completion)')
+
+init = arp.add_argument_group()
+base_dir_path = p.abspath(p.join(p.dirname(__file__), '../../base_indexes/'))
+large_base_path = p.join(base_dir_path, 'USE_large_base_IVF4K_15M.index')
+init.add_argument('-b', '--base_index', default=large_base_path,
+                  help='This path must point to the same base index used for '
+                       f'building the IVFs in mv_dir. (Default: {large_base_path})')
+
+zipped = arp.add_argument_group()
+zipped.add_argument('-p', '--PIN', '--partial-idx-name', default='zipped',
+                    help='Will be used to rename zipped indexes '
+                         '(i.e. YYYY-MM-DD_partial-idx-name.index). '
+                         'Only applies for --zip. (Default: `zipped`)')
+zipped.add_argument('-r', '--recursive', action='store_true', default=False,
+                    help='Flag to recursively collect faiss indexes '
+                         'nested under mv_dir. Only applies for --zip.')
 opts = arp.parse_args()
 # </editor-fold>
 
@@ -60,7 +67,7 @@ def main():
     else:
         idx_bdr.zip_indexes(
             mv_dir=p.abspath(opts.mv_dir), to_dir=p.abspath(opts.to_dir),
-            mkdir=True, recursive=opts.recursive
+            mkdir=True, partial_filename=opts.PIN, recursive=opts.recursive
         )
 
 

a25b653d28ba35f357d3fb52d35f090cb3b4f124
Lukas Ferrer <lukasjferrer@gmail.com>
2019-03-21 12:19:52 -0700

    Pipeline tmp: will move tmp_one_full_ingest.py into a shell script

--- one_shot_ingest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Requires conda env: dt_sim"
-
-python -u /faiss/dig-text-similarity-search/one_full_ingest.py;
---
+++ tmp_one_shot_ingest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Requires conda env: dt_sim"
+
+python -u /faiss/dig-text-similarity-search/auto-ingest/tmp_one_full_ingest.py;

68df5146d5e1e9e2e2029839e4751b21a084bf02
Lukas Ferrer <lukasjferrer@gmail.com>
2019-03-25 14:34:30 -0700

    Deprecated pub_date_split() --> gz_date_split()

--- py_scripts/preprocessing/sort_by_pub_date.py
+++ py_scripts/preprocessing/sort_by_pub_date.py
@@ -1,5 +1,6 @@
 # <editor-fold desc="Basic Imports">
 import os.path as p
+from datetime import date
 from argparse import ArgumentParser
 
 import sys
@@ -12,19 +13,24 @@ arp = ArgumentParser(description='Split a LexisNexis news_dump.jl '
                                  'by article publication dates.')
 arp.add_argument('input_file', help='Path to dirtyLexisNexis.jl to be sorted.')
 arp.add_argument('output_dir', help='Dir for saving cleanLexisNexis.jl files.')
-arp.add_argument('-c', '--cutoff_date', default='2018-01-01',
-                 help='Articles published after the cutoff date will be '
+arp.add_argument('-i', '--initial_date', default='2018-01-01',
+                 help='Articles published before the initial date will be '
                       'saved in output_dir/old_news/*.jl separately. '
                       '(Default: 2018-01-01)')
+arp.add_argument('-f', '--final_date', default=date.today(),
+                 help='Articles published after toe final date will be '
+                      'saved in output_dir/date_error/*.jl separately. '
+                      f'(Default is today: {date.today()})')
 opts = arp.parse_args()
 # </editor-fold>
 
-from dt_sim.data_reader.date_sort_funcs import pub_date_split
+from dt_sim.data_reader.date_sort_funcs import gz_date_split
 
 
 if __name__ == '__main__':
-    pub_date_split(
+    gz_date_split(
         input_file=opts.input_file,
         output_dir=opts.output_dir,
-        cutoff_date=opts.cutoff_date
+        first_date=opts.initial_date,
+        final_date=opts.final_date
     )

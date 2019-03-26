fdd0f375a424f3cf92cefcfc41ef6dd02cd1b6b0
Lukas Ferrer <lukasjferrer@gmail.com>
2019-03-26 16:02:35 -0700

    Removed unnecessary new line chars

--- s3cp.sh
+++ s3cp.sh
@@ -3,8 +3,7 @@
 FULL_PTH=$1
 
 printf "\nCopying $FULL_PTH
-To s3://lexisnexis-news-incremental$FULL_PTH \n
-(Note: Requires /full/path/to/file.ext) \n\n"
+To s3://lexisnexis-news-incremental$FULL_PTH \n"
 
 case "$FULL_PTH" in
         /*) pathchk -- "$FULL_PTH" && aws s3 cp "$FULL_PTH" s3://lexisnexis-news-incremental"$FULL_PTH";;

6ea6dc286b71c16fb8e3eaac4f4af61ebba6e44f
Lukas Ferrer <lukasjferrer@gmail.com>
2019-03-26 12:24:00 -0700

    Prints BEFORE & AFTER arrays line by line

--- ingest_news.sh
+++ ingest_news.sh
@@ -13,8 +13,8 @@ SERVICE="/faiss/dig-text-similarity-search/py_scripts/service/"
 MAIN_IDXS="/faiss/faiss_index_shards/deployment_full/"
 TMP_IDXS="/green_room/idx_deploy_B/"
 
-echo "Main: $(ls "$MAIN_IDXS" | wc -l) shards"
-echo "Tmp: $(ls "$TMP_IDXS" | wc -l) shards"
+echo "Main: $MAIN_IDXS has $(ls "$MAIN_IDXS" | wc -l) shards"
+echo "Tmp: $TMP_IDXS has $(ls "$TMP_IDXS" | wc -l) shards"
 
 # Get file to process
 FILE=$(ls "${NEWS_DIR}" | head -1)
@@ -83,12 +83,12 @@ python -u "${SERVICE}similarity_server.py" "$TMP_IDXS" -l -c 6 &
 
 # Zip-merge into main indexes
 cd "$MAIN_IDXS"
-BEFORE=$(*.i*); cd -; echo "$BEFORE"
+BEFORE=$(*.i*); cd -; printf "Before: %s\n" "${BEFORE[@]}"
 echo "n" | python -u "${PY_SCRIPTS}consolidate_shards.py" \
 "$DAILY_IDXS" "$MAIN_IDXS" --zip -p "zip_to_${MM}${DD}" -t 2;
 
 cd "$MAIN_IDXS"
-AFTER=$(*.i*); cd -; echo "$AFTER"
+AFTER=$(*.i*); cd -; printf "After: %s\n" "${AFTER[@]}"
 
 # Switch back to main service
 LOG_FILE="/faiss/dig-text-similarity-search/logs/service/deploy_${MM}${DD}.out"
@@ -114,7 +114,7 @@ echo "Saving new shards to s3..."
 B4=" ${BEFORE[*]} "
 for item in ${AFTER[@]}; do
     if [[ ! $B4 =~ " $item " ]]; then
-        /faiss/dig-text-similarity-search/s3cp.sh "${MAIN_IDXS}${item}"
+        /faiss/dig-text-similarity-search/s3cp.sh "${MAIN_IDXS}${item}";
     fi
 done
 
@@ -122,7 +122,7 @@ done
 AF7=" ${AFTER[*]} "
 for item in ${BEFORE[@]}; do
     if [[ ! $AF7 =~ " $item " ]]; then
-        /faiss/dig-text-similarity-search/s3rm.sh "${MAIN_IDXS}${item}"
+        /faiss/dig-text-similarity-search/s3rm.sh "${MAIN_IDXS}${item}";
     fi
 done
 

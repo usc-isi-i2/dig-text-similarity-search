de17395470f10412964e6b12b1e4c20a545ccc50
Lukas Ferrer <lukasjferrer@gmail.com>
2019-04-02 18:43:26 -0700

    Removed nested use of shell scripts

--- ingest_news.sh
+++ ingest_news.sh
@@ -18,12 +18,11 @@ echo "Main: $MAIN_IDXS has $(ls "$MAIN_IDXS"*.index | wc -l) shards"
 echo "Tmp: $TMP_IDXS has $(ls "$TMP_IDXS"*.index | wc -l) shards
 "
 
-# Get file to process
+# Process 1 file per call to shell script
 FILE=$(ls "${NEWS_DIR}" | head -1)
 
-# Get YYYY-MM-DD
+# Get extraction ISO date as YYYY, MM, & DD
 YYYYMMDD=$(echo "$FILE" | grep -Eo "[0-9]{4}\-[0-9]{2}\-[0-9]{2}")
-
 YYYY=$(echo "$YYYYMMDD" | cut -d "-" -f1)
 MM=$(echo "$YYYYMMDD" | cut -d "-" -f2)
 DD=$(echo "$YYYYMMDD" | cut -d "-" -f3)
@@ -34,31 +33,43 @@ echo "  * Year: $YYYY   * Month: $MM    * Day: $DD
 # New working dirs
 DATE_SPLIT="${PUB_DATES}${YYYY}_extraction_${MM}-${DD}/"
 DAILY_IDXS="${DAILY_DIR}${YYYY}_indexes_${MM}-${DD}/"
-mkdir "$DATE_SPLIT"
-mkdir "$DAILY_IDXS"
+mkdir "$DATE_SPLIT"     # Fail and exit if dirs exist
+mkdir "$DAILY_IDXS"     # Indication that file has already been processed
 
 
-#### TODO: Iterate over all files in NEWS_DIR
+
 ## Split
 echo "Splitting articles in $FILE by publication dates between
-$(date -d "-45 days $YYYYMMDD" -I) and $YYYYMMDD..."
+$(date -d "-45 days $YYYYMMDD" -I) and $YYYYMMDD"
+
 python -u "${PREPROC}sort_by_pub_date.py" \
 "${NEWS_DIR}${FILE}" "$DATE_SPLIT" \
 -i "$(date -d "-45 days $YYYYMMDD" -I)" -f "$YYYYMMDD";
 
-# Exit if there is nothing to vectorize
-n_shards=$(ls "$DATE_SPLIT"*.jl | wc -l)
-echo "Found $n_shards to vectorize
-"
 
 
-#### TODO: Iterate over all files in DATE_SPLIT
 ## Vectorize
-"$DT_SIM"vectorize_n_large_shards.sh \
-"$n_shards" "$DATE_SPLIT" "$DAILY_IDXS" "${DATE_SPLIT}progress.txt";
+n_shards=$(ls "$DATE_SPLIT"*.jl | wc -l)
+
+# Exit if there is nothing to vectorize
+if [[ "$n_shards" > 0 ]]; then
+    echo "Found $n_shards to vectorize
+"
+else
+    echo "Nothing to process... Exiting"
+    exit 1
+fi
+
+for i in `seq 1 "$n_shards"`; do
+	printf "\n\nStarting $i/$n_shards @ $(date)\n";
+	python -u py_scripts/preprocessing/prep_shard.py "$DATE_SPLIT" "$DAILY_IDXS" \
+	-p "${DATE_SPLIT}progress.txt" -b "${DT_SIM}base_indexes/USE_large_base_IVF4K_15M.index" \
+	-l -n 128 -v -t 2;      # Previously written to take dirs as inputs, not files
+done
 
 
-## Merge into indexes
+
+## Zip new indexes into deployed indexes
 # Switch to tmp service
 kill -15 $(ps -ef | grep "[s]imilarity_server" | awk \'{print $2}\'); sleep 1;
 python -u "${SERVICE}similarity_server.py" "$TMP_IDXS" -l -c 6 &
@@ -67,6 +78,7 @@ python -u "${SERVICE}similarity_server.py" "$TMP_IDXS" -l -c 6 &
 cd "$MAIN_IDXS"; BEFORE=(*.i*); cd -; printf "Before: %s\n" "${BEFORE[@]}"
 
 # Zip-merge into main indexes
+# Note: echo "n" prevents deleting new indexes before second zip
 echo "n" | python -u "${PREPROC}consolidate_shards.py" \
 "$DAILY_IDXS" "$MAIN_IDXS" --zip -p "zip_to_${MM}${DD}" -t 2;
 
@@ -78,12 +90,14 @@ LOG_FILE="/faiss/dig-text-similarity-search/logs/service/deploy_${MM}${DD}.out"
 kill -15 $(ps -ef | grep "[s]imilarity_server" | awk \'{print $2}\'); sleep 1;
 python -u "${SERVICE}similarity_server.py" "$MAIN_IDXS" -l -c 6 >> "$LOG_FILE" &
 
-# Zip-merge into tmp
+# Second zip-merge into tmp indexes
+# Note: echo "y" will delete new indexes
 echo "y" | python -u "${PREPROC}consolidate_shards.py" \
 "$DAILY_IDXS" "$TMP_IDXS" --zip -p "zip_to_${MM}${DD}" -t 2;
 
 
-## Save backup (new)
+
+## Save backups to S3
 echo "
 Saving new shards to s3..."
 
@@ -93,7 +107,7 @@ for item in ${AFTER[@]}; do
     if [[ ! $B4 =~ " $item " ]]; then
         echo "
         * $item not found in BEFORE :: backing up to s3... "
-        "$DT_SIM"s3cp.sh "${MAIN_IDXS}${item}";
+        aws s3 cp "${MAIN_IDXS}${item}" s3://lexisnexis-news-incremental"${MAIN_IDXS}${item}";
     fi
 done
 
@@ -103,9 +117,10 @@ for item in ${BEFORE[@]}; do
     if [[ ! $AF7 =~ " $item " ]]; then
         echo "
         * $item not found in AFTER :: attempting to remove from s3... "
-        "$DT_SIM"s3rm.sh "${MAIN_IDXS}${item}";
+        aws s3 rm s3://lexisnexis-news-incremental"${MAIN_IDXS}${item}";
     fi
 done
+
 
 
 ## Cleanup

fa7edad8addd171efcc2d0f9606b7a30f4d5a18b
Lukas J Ferrer <lukasjferrer@gmail.com>
2019-04-09 11:57:15 -0700

    Ingests news from 2.5 months ago (up from 1.5)

--- ingest_news.sh
+++ ingest_news.sh
@@ -51,10 +51,10 @@ fi
 
 ## Split
 echo "Splitting articles in $FILE by publication dates between
-$(date -d "-45 days $YYYYMMDD" -I) and $YYYYMMDD"
+$(date -d "-75 days $YYYYMMDD" -I) and $YYYYMMDD"
 
 python -u "${PREPROC}sort_by_pub_date.py" "${NEWS_DIR}${FILE}" "$DATE_SPLIT" \
--i "$(date -d "-45 days $YYYYMMDD" -I)" -f "$YYYYMMDD";
+-i "$(date -d "-75 days $YYYYMMDD" -I)" -f "$YYYYMMDD";
 
 
 

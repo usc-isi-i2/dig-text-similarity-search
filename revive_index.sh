1818921ae2a1b9331149d3d51aa7d075007ff0f5
Lukas Ferrer <lukasjferrer@gmail.com>
2019-04-16 14:59:32 -0700

    Prints contents of bucket before prompting user to proceed

--- revive_index.sh
+++ revive_index.sh
@@ -8,17 +8,16 @@ S3_BKUP="s3://lexisnexis-news-incremental$MAIN_IDXS"
 mkdir -p "$MAIN_IDXS"
 mkdir -p "$TMP_IDXS"
 
-n_main=$(ls "$MAIN_IDXS"*.index | wc -l)
-n_tmp=$(ls "$TMP_IDXS"*.index | wc -l)
-n_s3=$(aws s3 ls "$S3_BKUP" | wc -l)
 
-# Echo n indexes
-echo "Main: $MAIN_IDXS has $n_main shards"
-echo "Tmp:  $TMP_IDXS has $n_tmp shards"
-echo "S3:   $S3_BKUP has $n_s3 shards"
+# Echo n backed up and existing indexes
+n_main=$(ls "$MAIN_IDXS"*.i* | wc -l)
+n_tmp=$(ls "$TMP_IDXS"*.i* | wc -l)
+aws s3 ls "$S3_BKUP" --summarize --human-readable
+echo "Main: $MAIN_IDXS has $n_main index items"
+echo "Tmp:  $TMP_IDXS has $n_tmp index items"
 
 # Prompt user to agree to sync
-read -p "Found $n_main existing shards. Restoring the index may overwrite them. Continue? [y/N]" -n 1 -r
+read -p "Continue sync? [y/N]" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     cd "$MAIN_IDXS"
@@ -27,9 +26,12 @@ else
     exit 1
 fi
 
-# Prepare TMP Indexes
+# cd TMP and remove existing indexes
+cd "$TMP_IDXS" && rm *.i*
 ACTIVATE="/home/ubuntu/anaconda3/bin/activate"
-cd "$TMP_IDXS" && . "$ACTIVATE" dig_text_similarity
+. "$ACTIVATE" dig_text_similarity
 
-python -u /faiss/dig-text-similarity-search/py_scripts/preprocessing/consolidate_shards.py \
+# Prepare TMP Indexes
+DT_SIM="/faiss/dig-text-similarity-search/"
+python -u "${DT_SIM}py_scripts/preprocessing/consolidate_shards.py" \
 "$MAIN_IDXS" "$TMP_IDXS" -c -t 2        # Copy, N-Threads 2
